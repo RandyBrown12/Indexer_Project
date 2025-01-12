95699576dd3ba5506d1947491bf89fcadccf9935
Randy <randybrown9812@gmail.com>
2025-01-11 20:41:02 -0600

    Remove all unnecessary python files

--- test/test.py
+++ test/test.py
@@ -1,31 +1,22 @@
 import unittest
 from unittest.mock import patch
-from lib.utilities import decode_tx
+from ..lib.utilities import decode_tx
 from io import StringIO
 import os
 
-def greet(name):
-    print(f"Hello, {name}!")
-
 #file_path = os.getenv("FILE_PATH")
 #file_name = os.getenv("FILE_NAME")
-#output_path = os.getenv('txt')
 #num = os.getenv('x')
 
 class CheckRetriesForTestBlocks2(unittest.TestCase):
     
-    def setUp(self):
-        os.environ[''] = f'{os.pwd}/log'
-    
-
     def test(self):
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            greet("Alice")
-            self.assertEqual(mock_stdout.getvalue().strip(), "Hello, Alice!")
-        
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            greet("World")
-            self.assertEqual(mock_stdout.getvalue().strip(), "Hello, World!")
-    
-    def tearDown(self):
-        
+
+        ENV_VARIABLES = {
+            "FILE_PATH":'./test/test_blocks2',
+            "FILE_NAME":'7138068',
+            "x":'7138068'
+        }
+
+        with patch.dict(os.environ, ENV_VARIABLES):
+            decode_tx()
