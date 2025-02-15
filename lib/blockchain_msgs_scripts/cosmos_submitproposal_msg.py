8211bb6fc878804c10a80b909503802afbc00672
Randy <randybrown9812@gmail.com>
2025-02-15 17:09:29 -0600

    Fix typo

--- lib/blockchain_msgs_scripts/cosmos_submitproposal_msg.py
+++ lib/blockchain_msgs_scripts/cosmos_submitproposal_msg.py
@@ -7,7 +7,7 @@ Programming Language: Python 3.11                                               
                                                                                     *
 Libraries: json                                                                     *
                                                                                     *
-Creater Name: Randy Brown                                                           *
+Creator Name: Randy Brown                                                           *
                                                                                     *
 Published Date: 11/06/2024                                                          *
                                                                                     *
